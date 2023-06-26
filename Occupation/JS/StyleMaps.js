642103ad07051c44baa4a1806a9a206561dc3b8c
hassani1985 <40432395+hassani1985@users.noreply.github.com>
2023-06-26 23:15:10 +0100

    Add files via upload

--- Occupation/JS/StyleMaps.js
+++ Occupation/JS/StyleMaps.js
@@ -30,7 +30,7 @@ var coucheMaps={
 var legend=L.control.layers(listeMap,coucheMaps,{hideSingleBase:true}).addTo(carteO).false;
 
 
-L.control.scale({imperial:false,maxWidth:250,updateWhenIdle:true,sortLayers:true}).addTo(carteO)
+L.control.scale({imperial:false,maxWidth:100,updateWhenIdle:true,sortLayers:true,position: "bottomright"}).addTo(carteO)
 
 
 
