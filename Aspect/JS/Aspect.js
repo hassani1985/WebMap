1d79a6cf2115c0afab04b24f2a6ce3e85b7c6e8e
hassani1985 <40432395+hassani1985@users.noreply.github.com>
2023-07-05 21:23:03 +0100

    Add files via upload

--- Aspect/JS/Aspect.js
+++ Aspect/JS/Aspect.js
@@ -33,13 +33,17 @@ function styleAspect(feature) {
 
 var AspectVar =L.geoJson(Aspect,{style:styleAspect}).addTo(carteA)
 
-/*  code ajoter une legend des carte des donne alphapitique      */ 
+/*  code ajoter une legend des carte des donne alphapitique  */     
 
 function createLegend() {
     const legend = L.control({ position: 'bottomleft' });
   
     legend.onAdd = function (carteA) {
       const div = L.DomUtil.create('div', 'info legend');
+      var title = "Légende de la carte"; // Titre de la légende
+       
+         // Ajouter le titre de la légende
+         div.innerHTML += '<div class="legend-title">' + title + '</div>';
       const aspects = ["Plat", "Nord", "Est", "Sud", "Ouest"];
   
       for (let i = 0; i < aspects.length; i++) {
@@ -61,6 +65,65 @@ function createLegend() {
   const legendControl = createLegend();
   legendControl.addTo(carteA); // Assuming `map` is your Leaflet map object
   
+
+/*function createLegend() {
+  const legend = L.control({ position: 'bottomleft' });
+
+  legend.onAdd = function (map) {
+    const div = L.DomUtil.create('div', 'info legend');
+    const aspects = ["Plat", "Nord", "Est", "Sud", "Ouest"];
+    let legendVisible = true; // Track the visibility state of the legend
+
+    for (let i = 0; i < aspects.length; i++) {
+      const aspect = aspects[i];
+      const backgroundColor = AspectColor(aspect); // Assuming AspectColor() function returns the background color for the aspect
+
+      const squareHTML = `<i style="background:${backgroundColor}"></i>`;
+      const labelHTML = aspect;
+
+      div.innerHTML += squareHTML + ' ' + labelHTML + '<br>';
+    }
+
+    // Create a "Masquer" button to hide the legend
+    const hideButton = L.DomUtil.create('button', 'hide-button');
+    hideButton.innerHTML = 'Masquer';
+
+    hideButton.addEventListener('click', function () {
+      div.style.display = 'none'; // Hide the legend div
+      legendVisible = false;
+    });
+
+    div.appendChild(hideButton); // Append the "Masquer" button to the legend div
+
+    // Create a button on the map to show the hidden legend
+    const showButton = L.DomUtil.create('button', 'show-button');
+    showButton.innerHTML = 'Afficher';
+
+    showButton.addEventListener('click', function () {
+      div.style.display = 'block'; // Show the legend div
+      legendVisible = true;
+    });
+
+    // Create a control button and add it to the map
+    const controlContainer = L.DomUtil.create('div', 'control-container');
+    controlContainer.appendChild(showButton);
+
+    const controlButton = L.control({ position: 'topleft' });
+    controlButton.onAdd = function () {
+      return controlContainer;
+    };
+
+    controlButton.addTo(map);
+
+    return div;
+  };
+
+  return legend;
+}
+
+const legendControl = createLegend();
+legendControl.addTo(carteA); // Assuming `carteA` is your Leaflet map object
+*/
 
 
 /*   Riseau hydraulique */
@@ -90,8 +153,6 @@ var ZonesAspect =  L.geoJSON(ZonesA,{
 /*var Name = L.geoJSON(NameZone,).bindPopup(function(layer){
   return layer.feature.properties.Name; 
 }).addTo(carte).openPopup()*/
-
-
 
 
 /**  Control */
